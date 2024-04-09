e3400953420f321cab16f8310dece456f8c3d159
Ranjan kashyap <kashyapranjan9977@gmail.com>
2024-04-09 17:12:22 +0530

    changesss

--- app/Pricing/page.js
+++ app/Pricing/page.js
@@ -1,13 +1,61 @@
-import React from "react";
-function price () {
-  return ( 
-    <div className="page">
-        <p>Pricing</p>
-        <h4>1000</h4>
+'use client'
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos a aut tenetur? Blanditiis fugiat nostrum earum soluta aliquam architecto dolorum?</p>
-    </div>
-   );
+import React from "react";
+import Accordion from 'react-bootstrap/Accordion';
+
+function price() {
+  return (
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is React</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>What is Next.js</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>What is Framework</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
 }
 
-export default  price;
+export default price;
+// function price () {
+//   return ( 
+//     <div className="page">
+//         <p>Pricing</p>
+//         <h4>1000</h4>
+
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos a aut tenetur? Blanditiis fugiat nostrum earum soluta aliquam architecto dolorum?</p>
+//     </div>
+//    );
+// }
+
+// export default  price;
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,64 @@
+"use client"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Link  from 'next/link'
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function header() {
-    return(
-    <div className="header">
-        <p>logo</p>
-        <nav>
-<Link href="/">Home</Link>
-<Link href="/About">About</Link>
-<Link href="/Contact">Contact</Link>
-<Link href="/Pricing">Pricing</Link>
-        </nav>
-    </div>
-    )
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Next.js</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link href="/">Home</Link>
+            <Link href="/About">About</Link>
+            <Link href="/Contact">Contact</Link>
+            <Link href="/Pricing">Pricing</Link>
+          
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default header;
+
+
+// function header() {
+//     return(
+//     <div className="header">
+//         <p>logo</p>
+//         <nav>
+// <Link href="/">Home</Link>
+// <Link href="/About">About</Link>
+// <Link href="/Contact">Contact</Link>
+// <Link href="/Pricing">Pricing</Link>
+//         </nav>
+//     </div>
+//     )
+// }
+
+// export default header;
